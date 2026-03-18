e7176c0db45bc4368fc2396fcc360aa622306d54
ELamraniG <137711176+ELamraniG@users.noreply.github.com>
2026-03-18 09:10:55 +0100

    start assomanager

---
+++ Assomanager/My Project/AssemblyInfo.vb
@@ -0,0 +1,20 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+
+<Assembly: AssemblyTitle("Assomanager")>
+<Assembly: AssemblyDescription("")>
+<Assembly: AssemblyCompany("")>
+<Assembly: AssemblyProduct("Assomanager")>
+<Assembly: AssemblyCopyright("Copyright ©  2025")>
+<Assembly: AssemblyTrademark("")>
+
+<Assembly: ComVisible(False)>
+
+
+<Assembly: Guid("61347b4c-ee19-447a-b571-80ec60716a9e")>
+
+
+<Assembly: AssemblyVersion("1.0.0.0")>
+<Assembly: AssemblyFileVersion("1.0.0.0")>
