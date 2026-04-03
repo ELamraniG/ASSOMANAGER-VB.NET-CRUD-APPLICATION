3dfe8ef46d56798830cf39186a7ad1dd690baefd
ELamraniG <137711176+ELamraniG@users.noreply.github.com>
2026-04-03 11:00:25 +0100

    dialogs

---
+++ Assomanager/Form26.vb
@@ -0,0 +1,32 @@
+﻿Imports System.Windows.Forms
+Imports System.IO
+
+Public Class Form26
+
+    Private Sub OK_Button_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles OK_Button.Click
+        Me.DialogResult = System.Windows.Forms.DialogResult.OK
+        Me.Close()
+    End Sub
+
+    Private Sub Cancel_Button_Click(ByVal sender As System.Object, ByVal e As System.EventArgs)
+        Me.DialogResult = System.Windows.Forms.DialogResult.Cancel
+        Me.Close()
+    End Sub
+
+    Private Sub Form26_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
+        Me.Text = "Politique de confidentialité"
+
+        Dim chemin As String = "aides/politique de confidentialité.txt"
+
+
+        If File.Exists(chemin) Then
+
+            Dim content As String = File.ReadAllText(chemin)
+
+
+            TextBox1.Text = content
+        Else
+            MessageBox.Show("Le fichier texte n'existe pas.")
+        End If
+    End Sub
+End Class
---
+++ Assomanager/Form28.Designer.vb
@@ -0,0 +1,48 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form28
+    Inherits System.Windows.Forms.Form
+
+
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+
+    Private components As System.ComponentModel.IContainer
+
+
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.WebBrowser1 = New System.Windows.Forms.WebBrowser()
+        Me.SuspendLayout()
+
+
+        Me.WebBrowser1.Anchor = CType((((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Bottom) _
+                    Or System.Windows.Forms.AnchorStyles.Left) _
+                    Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.WebBrowser1.Location = New System.Drawing.Point(229, 64)
+        Me.WebBrowser1.Margin = New System.Windows.Forms.Padding(3, 2, 3, 2)
+        Me.WebBrowser1.MinimumSize = New System.Drawing.Size(18, 16)
+        Me.WebBrowser1.Name = "WebBrowser1"
+        Me.WebBrowser1.Size = New System.Drawing.Size(549, 369)
+        Me.WebBrowser1.TabIndex = 2
+
+
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(1006, 496)
+        Me.Controls.Add(Me.WebBrowser1)
+        Me.Name = "Form28"
+        Me.Text = "Form28"
+        Me.ResumeLayout(False)
+
+    End Sub
+    Friend WithEvents WebBrowser1 As System.Windows.Forms.WebBrowser
+End Class
